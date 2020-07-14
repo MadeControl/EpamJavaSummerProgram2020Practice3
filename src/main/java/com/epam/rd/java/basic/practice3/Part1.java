b8449c84484dfa6ce1ba8bb28383d27ce218edde
MadeControl <danya.polishchuk.2001@ukr.ent>
2020-07-14 16:28:31 +0300

    changed Part1(2): convert1, convert2

--- src/main/java/com/epam/rd/java/basic/practice3/Part1.java
+++ src/main/java/com/epam/rd/java/basic/practice3/Part1.java
@@ -5,7 +5,6 @@ import java.util.regex.Pattern;
 
 public class Part1 {
 
-    private static final String TEXT = Util.readFile("part1.txt");
     private static Pattern pattern;
     private static Matcher matcher;
     private static String regex;
@@ -13,12 +12,10 @@ public class Part1 {
 
     public static void main(String[] args) {
 
-        Part1.convert1(TEXT);
-
     }
 
     public static String convert1(String input) {
-        regex = "(.+);(.+);(.+)";
+        regex = "(.+);((.+)\\s(.+));(([.[^@\\s]]+)@([.[^@\\s]]+)\\.([a-z]+))";
         pattern = Pattern.compile(regex);
         stringBuilder = new StringBuilder();
         String[] texts = input.split("\n");
@@ -26,14 +23,14 @@ public class Part1 {
         for(String newString : texts){
             matcher = pattern.matcher(newString);
             while (matcher.find()){
-                stringBuilder.append(matcher.group(1)).append(": ").append(matcher.group(3)).append("\n");
+                stringBuilder.append(matcher.group(1)).append(": ").append(matcher.group(5)).append("\n");
             }
         }
         return stringBuilder.toString();
     }
 
     public static String convert2(String input) {
-        regex = "(.+);(.+);(.+)";
+        regex = "(.+);((.+)\\s(.+));(([.[^@\\s]]+)@([.[^@\\s]]+)\\.([a-z]+))";
         pattern = Pattern.compile(regex);
         stringBuilder = new StringBuilder();
         String[] texts = input.split("\n");
@@ -41,9 +38,8 @@ public class Part1 {
         for(String newString : texts){
             matcher = pattern.matcher(newString);
             while (matcher.find()){
-                String[] array = matcher.group(2).split("\\s");
                 stringBuilder
-                        .append(array[1]).append(" ").append(array[0]) // It will be Surname Name(example: Ivanov Ivan)
+                        .append(matcher.group(2)) // It will be Surname Name(example: Ivanov Ivan)
                         .append(" (email: ").append(matcher.group(3))
                         .append(")\n");
             }
