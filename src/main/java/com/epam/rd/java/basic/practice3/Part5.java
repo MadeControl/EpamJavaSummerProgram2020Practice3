028b08995883bc800988d886eb338084a4de692b
MadeControl <danya.polishchuk.2001@ukr.ent>
2020-07-14 23:04:19 +0300

    added Part5

--- src/main/java/com/epam/rd/java/basic/practice3/Part5.java
+++ src/main/java/com/epam/rd/java/basic/practice3/Part5.java
@@ -3,14 +3,70 @@ package com.epam.rd.java.basic.practice3;
 public class Part5 {
 
     public static void main(String[] args) {
-        
+        final String arrow = " --> ";
+        for(int i = 1; i <= 100; i++){
+            System.out.println(i+arrow+decimal2Roman(i)+arrow+roman2Decimal(decimal2Roman(i)));
+        }
+
     }
 
     public static String decimal2Roman(int dec) {
-        return null;
+        if( dec <= 0) {
+            throw new IllegalArgumentException();
+        }
+
+        StringBuilder stringBuilder = new StringBuilder();
+        final Numeral[] values = Numeral.values();
+
+        for (int i = values.length - 1; i >= 0; i--) {
+            while (dec >= values[i].weight) {
+                stringBuilder.append(values[i]);
+                dec -= values[i].weight;
+            }
+        }
+        return stringBuilder.toString();
     }
 
     public static int roman2Decimal(String roman) {
-        return 0;
+        int result = 0;
+
+        for(int i = 0; i < roman.length()-1; i++) {
+            if (number(roman.charAt(i)) < number(roman.charAt(i+1))) {
+                result -= number(roman.charAt(i));
+            } else {
+                result += number(roman.charAt(i));
+            }
+        }
+        result += number(roman.charAt(roman.length()-1));
+        return result;
+    }
+
+    private static int number(char letter) {
+        switch(letter) {
+            case 'C': return 100;
+            case 'L': return 50;
+            case 'X': return 10;
+            case 'V': return 5;
+            case 'I': return 1;
+            default: return 0;
+        }
+    }
+
+    private enum Numeral {
+        I(1),
+        IV(4),
+        V(5),
+        IX(9),
+        X(10),
+        XL(40),
+        L(50),
+        XC(90),
+        C(100);
+
+        int weight;
+
+        Numeral(int weight) {
+            this.weight = weight;
+        }
     }
 }
--- src/test/java/com/epam/rd/java/basic/practice3/Part5Test.java
+++ src/test/java/com/epam/rd/java/basic/practice3/Part5Test.java
@@ -1,5 +1,36 @@
 package com.epam.rd.java.basic.practice3;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class Part5Test {
+
+    @Test
+    public void decimal2RomanTest(){
+
+        Assert.assertEquals("I", Part5.decimal2Roman(1));
+        Assert.assertEquals("IV", Part5.decimal2Roman(4));
+        Assert.assertEquals("V", Part5.decimal2Roman(5));
+        Assert.assertEquals("X", Part5.decimal2Roman(10));
+        Assert.assertEquals("XL", Part5.decimal2Roman(40));
+        Assert.assertEquals("L", Part5.decimal2Roman(50));
+        Assert.assertEquals("XC", Part5.decimal2Roman(90));
+        Assert.assertEquals("C", Part5.decimal2Roman(100));
+
+    }
+
+    @Test
+    public void roman2DecimalTest(){
+
+        Assert.assertEquals(1, Part5.roman2Decimal("I"));
+        Assert.assertEquals(4, Part5.roman2Decimal("IV"));
+        Assert.assertEquals(5, Part5.roman2Decimal("V"));
+        Assert.assertEquals(10, Part5.roman2Decimal("X"));
+        Assert.assertEquals(40, Part5.roman2Decimal("XL"));
+        Assert.assertEquals(50, Part5.roman2Decimal("L"));
+        Assert.assertEquals(90, Part5.roman2Decimal("XC"));
+        Assert.assertEquals(100, Part5.roman2Decimal("C"));
+
+    }
 
 }
